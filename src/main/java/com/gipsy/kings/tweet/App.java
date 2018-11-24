119a25ceb1d06c9d6f1bbfc5258dce916bc6ea37
yasserm13 <32194103+yasserm13@users.noreply.github.com>
2018-11-24 16:29:06 +0100

    Add files via upload
    Ajout du template projet type maven

---
+++ src/main/java/com/gipsy/kings/tweet/App.java
@@ -0,0 +1,13 @@
+package com.gipsy.kings.tweet;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
