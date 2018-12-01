12c41a85f6e3be8c4de3e8377da50a94c30710e5
cesar <cesarbernedo@gmail.com>
2018-12-01 14:46:33 +0100

    Creer controleurTweet

---
+++ src/main/java/com/gipsy/kings/tweet/controller/TweetController.java
@@ -0,0 +1,68 @@
+package src.main.java.com.gipsy.kings.tweet.controller;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.inject.Model;
+import javax.enterprise.inject.Produces;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import com.gipsy.kings.tweet.model.Member;
+import com.gipsy.kings.tweet.service.MemberRegistration;
+
+// The @Model stereotype is a convenience mechanism to make this a request-scoped bean that has an
+// EL name
+// Read more about the @Model stereotype in this FAQ:
+// http://www.cdi-spec.org/faq/#accordion6
+@Model
+public class TweetController {
+
+    @Inject
+    private FacesContext facesContext;
+
+    @Inject
+    private TweetRegistration tweetRegistration;
+
+    @Produces
+    @Named
+    private Tweet newTweet;
+
+    @PostConstruct
+    public void initNewTweet() {
+        newTweet = new Tweet();
+    }
+
+    public void register() throws Exception {
+        try {
+            tweetRegistration.register(newTweet);
+            FacesMessage m = new FacesMessage(FacesMessage.SEVERITY_INFO, "Registered!", "Registration successful");
+            facesContext.addMessage(null, m);
+            initNewTweet();
+        } catch (Exception e) {
+            String errorMessage = getRootErrorMessage(e);
+            FacesMessage m = new FacesMessage(FacesMessage.SEVERITY_ERROR, errorMessage, "Registration unsuccessful");
+            facesContext.addMessage(null, m);
+        }
+    }
+
+    private String getRootErrorMessage(Exception e) {
+        // Default to general error message that registration failed.
+        String errorMessage = "Registration failed. See server log for more information";
+        if (e == null) {
+            // This shouldn't happen, but return the default messages
+            return errorMessage;
+        }
+
+        // Start with the exception and recurse to find the root cause
+        Throwable t = e;
+        while (t != null) {
+            // Get the message from the Throwable class instance
+            errorMessage = t.getLocalizedMessage();
+            t = t.getCause();
+        }
+        // This is the root cause message
+        return errorMessage;
+    }
+
+}
