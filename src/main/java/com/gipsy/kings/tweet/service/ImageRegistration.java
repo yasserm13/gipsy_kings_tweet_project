570590be5fb651b98b5c8d1739b51a8f911cd865
cesar <cesarbernedo@gmail.com>
2018-12-08 14:12:03 +0100

    rajout de la registration des images

---
+++ src/main/java/com/gipsy/kings/tweet/service/ImageRegistration.java
@@ -0,0 +1,34 @@
+package com.gipsy.kings.tweet.service;
+
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.time.Instant;
+
+import javax.imageio.ImageIO;
+
+public class ImageRegistration {
+	
+	
+	public final String PATH = "/opt/jboss/images";
+	
+	
+	public long register(long senderID,byte[] data) throws IOException {
+		
+		long timestmp = Instant.now().toEpochMilli();
+		
+		// convert byte array back to BufferedImage
+		InputStream in = new ByteArrayInputStream(data);
+		BufferedImage nvImage = ImageIO.read(in);
+		
+		if ((nvImage.getHeight() > 500) || (nvImage.getWidth() > 500 ))
+			nvImage = (BufferedImage) nvImage.getScaledInstance(500, 500, java.awt.Image.SCALE_DEFAULT);
+		
+		ImageIO.write(nvImage,"JPG" ,new File(PATH + "/" + senderID + "/" + timestmp + ".jpg"));
+		
+		return timestmp;
+	}
+
+}
