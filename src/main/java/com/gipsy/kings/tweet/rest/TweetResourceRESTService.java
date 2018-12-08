136406037014fe2c5add431ebfc2584e8747aa70
cesar <cesarbernedo@gmail.com>
2018-12-08 11:53:57 +0100

    Mise a jour spec et fonction post et get avec gestion des erreurs

--- src/main/java/com/gipsy/kings/tweet/rest/TweetResourceRESTService.java
+++ src/main/java/com/gipsy/kings/tweet/rest/TweetResourceRESTService.java
@@ -79,18 +79,19 @@ public class TweetResourceRESTService {
             // Create an "ok" response
             builder = Response.ok();
         } catch (ConstraintViolationException ce) {
-            // Handle bean validation issues
+            // Handle bean validation issues typiquement tweet trop long ou un type qui va pas un non null ?
+        	// il s'agit d'une erreur 406 je l'ai modififie dans createViolationResponse
             builder = createViolationResponse(ce.getConstraintViolations());
         } catch (ValidationException e) {
-            // Handle the unique constrain violation
+            // Handle the unique constrain violation tweetid existe deja => ne devrait jamais arrivé
             Map<String, String> responseObj = new HashMap<String, String>();
-            responseObj.put("email", "Email taken");
-            builder = Response.status(Response.Status.CONFLICT).entity(responseObj);
+            responseObj.put("tweetid", "tweetid existe déjà");
+            builder = Response.status(Response.Status.NOT_ACCEPTABLE).entity(responseObj);
         } catch (Exception e) {
-            // Handle generic exceptions
+            // Handle generic exceptions = 
             Map<String, String> responseObj = new HashMap<String, String>();
-            responseObj.put("error", e.getMessage());
-            builder = Response.status(Response.Status.BAD_REQUEST).entity(responseObj);
+            responseObj.put("erreur", e.getMessage());
+            builder = Response.status(Response.Status.NOT_ACCEPTABLE).entity(responseObj);
         }
 
         return builder.build();
@@ -102,13 +103,13 @@ public class TweetResourceRESTService {
      * bean validation errors then it will throw a ConstraintValidationException with the set of the constraints violated.
      * </p>
      * <p>
-     * If the error is caused because an existing member with the same email is registered it throws a regular validation
+     * If the error is caused because an existing member with the same tweetid is registered it throws a regular validation
      * exception so that it can be interpreted separately.
      * </p>
      * 
      * @param member Member to be validated
      * @throws ConstraintViolationException If Bean Validation errors exist
-     * @throws ValidationException If member with the same email already exists
+     * @throws ValidationException If member with the same tweetid already exists
      */
     private void validateMember(Tweet tweet) throws ConstraintViolationException, ValidationException {
         // Create a bean validator and check for issues.
@@ -123,6 +124,7 @@ public class TweetResourceRESTService {
     /**
      * Creates a JAX-RS "Bad Request" response including a map of all violation fields, and their message. This can then be used
      * by clients to show violations.
+     * Renvoie une erreur 406 avec les violations
      * 
      * @param violations A set of violations that needs to be reported
      * @return JAX-RS response containing all violations
@@ -136,7 +138,7 @@ public class TweetResourceRESTService {
             responseObj.put(violation.getPropertyPath().toString(), violation.getMessage());
         }
 
-        return Response.status(Response.Status.BAD_REQUEST).entity(responseObj);
+        return Response.status(Response.Status.NOT_ACCEPTABLE).entity(responseObj);
     }
 
 
